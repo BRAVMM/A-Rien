0dfc2e86ea94a25a87cd271a006d71593c75f785
Mathéo Martin <matheo.martin@epitech.eu>
2023-12-06 16:09:52 +0100

    Front: remove template according with @badidu09 MINOR

---
+++ front/app/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Alert, Button, Text, View} from 'react-native';
+
+export default function Hello() {
+  return (
+    <View>
+      <Text>Hello, world!</Text>
+    </View>
+  );
+}
