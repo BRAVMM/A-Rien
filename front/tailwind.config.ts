3e70060a6994e73d7fc8b35c95b9b4f633bd04ec
Mathéo Martin <matheo.martin@epitech.eu>
2023-12-08 01:12:36 +0100

    Front: Add the register page (need to implement the script to communicate with the backend) MAJOR

--- front/tailwind.config.ts
+++ front/tailwind.config.ts
@@ -7,8 +7,36 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'primary': '#0B0534',
+      'secondary': '#382B59',
+      'thirdly': '#4F3E6C',
+      'fourthly': '#65517E',
+      'backgroundsecondary': '#BE9CC7',
+      'background': '#404040',
+      'white': '#FFFFFF',
+    },
     extend: {
+      inset: {
+        '1/6': '5%',
+        '7/10': '70%',
+        '1/10': '10%',
+        '5/100': '5%',
+        '3/100': '3%',
+      },
+      padding: {
+        '1/2': '50%',
+      },
+      margin: {
+        '1/2': '50%',
+        '1/4': '25%',
+        '1/6': '16.666667%',
+        '1/10': '10%',
+      },
       backgroundImage: {
+        'custom-gradient': 'linear-gradient(115deg, #404040 40%, #0B0534 65%)',
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
