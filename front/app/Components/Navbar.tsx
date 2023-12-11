f92e7dbf1451f9bb83d5fb825148dbe87bb00d94
badidu09 <vincent.ballandi@epitech.eu>
2023-12-11 10:50:30 +0100

    FRONT: Change nav style
    Change nav style & add redirect on button
    
    MINOR

--- front/app/Components/Navbar.tsx
+++ front/app/Components/Navbar.tsx
@@ -1,19 +1,17 @@
-import React from 'react';
-import Image from 'next/image'; // Import de next/image
+import React from "react";
+import Image from "next/image"; // Import de next/image
 
 function Navbar() {
   return (
     <nav className="bg-transparent p-4">
-      <div className="container mx-auto">
-        <div className="flex items-center justify-center">
-          <ul className="flex space-x-4 sm:space-x-8 lg:space-x-32 items-center justify-center">
-            <Image src="/logo1.svg" alt="Logo" width={100} height={100} />
-            <li className="text-white text-xl">Product</li>
-            <li className="text-white text-xl">Forums</li>
-            <li className="text-white text-xl">Documentation</li>
-            <li className="text-white text-xl">FAQ</li>
-          </ul>
-        </div>
+      <div className="container mx-auto flex items-center justify-between">
+        <Image src="/logo1.svg" alt="Logo" width={100} height={100} />
+        <ul className="flex-grow flex space-x-4 sm:space-x-8 lg:space-x-32 items-center justify-center"> {/* Utibuttonsez flex-grow pour centrer la buttonste */}
+          <button className="text-white text-xl">Product</button>
+          <button className="text-white text-xl">Forums</button>
+          <button className="text-white text-xl">Documentation</button>
+          <button className="text-white text-xl">FAQ</button>
+        </ul>
       </div>
     </nav>
   );
