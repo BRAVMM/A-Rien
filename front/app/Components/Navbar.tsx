574578d2f6ffbb9f06bdcae44ce4e6222a38c5ef
badidu09 <vincent.ballandi@epitech.eu>
2023-12-12 17:00:09 +0100

    FRONT: Add services pages
    Add services pages
    
    MINOR

--- front/app/Components/Navbar.tsx
+++ front/app/Components/Navbar.tsx
@@ -1,13 +1,23 @@
 import React from "react";
+import { useRouter } from 'next/navigation';
 import Image from "next/image"; // Import de next/image
 
 function Navbar() {
+
+  const router = useRouter();
+
+  const handleProductClick = () => {
+    router.push('/services');
+  }
+
   return (
     <nav className="bg-transparent p-4">
       <div className="container mx-auto flex items-center justify-between">
         <Image src="/logo1.svg" alt="Logo" width={100} height={100} />
         <ul className="flex-grow flex space-x-4 sm:space-x-8 lg:space-x-32 items-center justify-center"> {/* Utibuttonsez flex-grow pour centrer la buttonste */}
-          <button className="text-white text-xl">Product</button>
+          <button
+            onClick={handleProductClick}
+          className="text-white text-xl">Product</button>
           <button className="text-white text-xl">Forums</button>
           <button className="text-white text-xl">Documentation</button>
           <button className="text-white text-xl">FAQ</button>
