a777f2496b738bf768b1b412bce818bc607e81a3
Vincent <72189942+badidu09@users.noreply.github.com>
2024-01-02 10:09:47 +0100

    Update mobile/app/_layout.tsx
    Co-authored-by: Macéo JALBERT <42516894+Vivelis@users.noreply.github.com>

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,8 +1,6 @@
 import { Stack } from 'expo-router';
 import login from './login';
 import index from './index';
-
-
 export default function Layout() {
   return <Stack initialScreen="login" screens={{ login, index }} screenOptions={
     {
