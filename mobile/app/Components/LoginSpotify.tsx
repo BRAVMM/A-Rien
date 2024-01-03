f49c292a3b3732dd0f130660f32ccfe42a6ef9f3
Kitetsu <brice.desousa@epitech.eu>
2024-01-03 21:42:46 +0100

    FRONT: Modif now code is generated correctly MINOR

--- mobile/app/Components/LoginSpotify.tsx
+++ mobile/app/Components/LoginSpotify.tsx
@@ -1,9 +1,12 @@
 import * as React from 'react';
 import * as WebBrowser from 'expo-web-browser';
-import { makeRedirectUri, useAuthRequest } from 'expo-auth-session';
+import { makeRedirectUri, useAuthRequest, Prompt } from 'expo-auth-session';
 import { Button } from 'react-native';
+import { registerTokenService } from '../Services/callApi';
+import { SpotifyDataBody } from '../Interface/dataBody.interface';
 
-// Endpoint
+const REGISTER_TOKEN_ROUTE = "/services/spotify/registerToken";
+
 const discovery = {
   authorizationEndpoint: 'https://accounts.spotify.com/authorize',
   tokenEndpoint: 'https://accounts.spotify.com/api/token',
@@ -17,19 +20,33 @@ export default function App() {
           clientId: clientID,
           scopes: ['user-read-email', 'playlist-modify-public'],
           usePKCE: false,
+          extraParams: {
+            show_dialog: 'true'
+          },
           redirectUri: makeRedirectUri({
             scheme: 'myapp'
           }),
         },
         discovery,
-
     );
+
+    const apiCall = async (code: string) => {
+      try {
+        // await registerTokenService(new SpotifyDataBody(code), REGISTER_TOKEN_ROUTE);
+      } catch (error) {
+        console.log(error)
+      }
+    } 
 
     React.useEffect(() => {
       if (response?.type === 'success') {
         const { code } = response.params;
 
-        console.log(code)
+        try {
+          apiCall(code)
+        } catch(error) {
+            console.log(error)
+        }
       }
     }, [response]);
 
