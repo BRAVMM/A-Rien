d4116fefafa888ea67d409339e8983eef093b3ab
vincent <vincent.ballandi@epitech.eu>
2024-01-08 17:50:12 +0100

    FRONT: Keep code to oauth
    Add state with oauth code
    
    MINOR

--- front/app/redirect/page.tsx
+++ front/app/redirect/page.tsx
@@ -1,16 +1,25 @@
 "use client"
 import { useRouter } from 'next/router';
 import { useEffect, useState } from "react";
+import { registerTokenService } from '../Utils/callApi';
+import { MicroSoftDataBody } from '../Interfaces/dataBody.interface';
 
 export default function Redirect() {
 
-    const [code, setCode] = useState<string | null>(null);
+    const [code, setCode] = useState<string>("null");
 
     useEffect(() => {
       const queryParams = new URLSearchParams(window.location.hash.substring(1)); // remove the '#' at start
-      const codeFound = queryParams.get('code');
+      const codeFound: string = queryParams.get('code') ?? "";
       setCode(codeFound);
       console.log(codeFound);
+      const handleSetData = async () => {
+        const res = await registerTokenService(new MicroSoftDataBody(codeFound), "/Se/microsoft/registerToken");
+        console.log(res);
+      }
+      if (codeFound !== "") {
+        handleSetData();
+      }
     }, []);
 
 
