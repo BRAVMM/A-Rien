fcb67cecace46e2b5a4d0a8387fad143c8661775
vincent <vincent.ballandi@epitech.eu>
2024-01-08 17:37:24 +0100

    FRONT: Keep code to oauth
    Add state with oauth code
    
    MINOR

--- front/app/redirect/page.tsx
+++ front/app/redirect/page.tsx
@@ -1,20 +1,22 @@
 "use client"
-'use client'
-import {  useRouter } from 'next/navigation';
-import { useEffect } from "react";
+import { useRouter } from 'next/router';
+import { useEffect, useState } from "react";
 
 export default function Redirect() {
 
+    const [code, setCode] = useState<string | null>(null);
 
     useEffect(() => {
-        const queryParams = window.location.hash
-        console.log(queryParams)
+      const queryParams = new URLSearchParams(window.location.hash.substring(1)); // remove the '#' at start
+      const codeFound = queryParams.get('code');
+      setCode(codeFound);
+      console.log(codeFound);
     }, []);
-  
+
 
   return (
     <div className="relative h-screen">
-
+      {code && <div>Code: {code}</div>}
     </div>
-);/*  */
+  );
 }
