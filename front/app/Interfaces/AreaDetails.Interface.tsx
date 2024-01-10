f75504c819d9dfc5e6e8707e02f18551bc6c456c
Viveli <maceo.jalbert@epitech.eu>
2024-01-10 14:41:38 +0100

    FRONT: change codeRabbit suggestions
    - change codeRabbit suggestions
    
    PATCH

--- front/app/Interfaces/AreaDetails.Interface.tsx
+++ front/app/Interfaces/AreaDetails.Interface.tsx
@@ -8,7 +8,7 @@
  * @description Interface for AreaDetailsInterface.
  * @property {number} id - Id of the AreaDetailsInterface.
  * @property {string} title - Name of the AreaDetailsInterface.
- * @property {string} actionName - Name of the action of the AreaDetailsInterface.
+ * @property {string} serviceName - Name of the service of the AreaDetailsInterface.
  * @property {boolean} isActivated - Status of the AreaDetailsInterface.
  */
 interface AreaDetailsInterface {
