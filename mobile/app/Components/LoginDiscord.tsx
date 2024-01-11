0a18c5fa136fe9482bd37cabc289ea8cc66eb4b3
Viveli <maceo.jalbert@epitech.eu>
2024-01-11 13:55:31 +0100

    MOBILE: Add LoginDiscord
    - Add LoginDiscord
    
    MINOR

---
+++ mobile/app/Components/LoginDiscord.tsx
@@ -0,0 +1,74 @@
+import * as React from 'react';
+import { makeRedirectUri, useAuthRequest } from 'expo-auth-session';
+import { Button } from 'react-native';
+import { useRoute } from '@react-navigation/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const REGISTER_TOKEN_ROUTE = "/services/discord/registerToken";
+
+const discovery = {
+    authorizationEndpoint: 'https://discord.com/oauth2/authorize',
+    tokenEndpoint: 'https://accounts.discord.com/api/token',
+};
+
+export default function App() {
+    const route = useRoute()
+    const clientID: string = process.env.EXPO_PUBLIC_DISCORD_CLIENT_ID ?? ''
+
+    const [request, response, promptAsync] = useAuthRequest(
+        {
+            clientId: clientID,
+            scopes: ['webhook.incoming'],
+            usePKCE: false,
+            extraParams: {
+                show_dialog: 'true'
+            },
+            redirectUri: makeRedirectUri({
+                scheme: 'myapp',
+                path: 'home'
+            }),
+        },
+        discovery,
+    );
+
+    const apiCall = async (code: string, guildId: string) => {
+        try {
+            const bearer = await AsyncStorage.getItem("token")
+
+            const response = await fetch(process.env.EXPO_PUBLIC_API_URL + REGISTER_TOKEN_ROUTE, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${bearer}`,
+                },
+                body: JSON.stringify({ code: code, guildId: guildId, mobile: true }),
+            });
+            if (!response.ok) {
+                const error = await response.json();
+                throw new Error(error.error);
+            }
+        } catch (error) {
+            console.log(error)
+            return
+        }
+    }
+
+    React.useEffect(() => {
+        if (response?.type === 'success') {
+            const { code, guildId } = response.params;
+
+            apiCall(code, guildId)
+        }
+    }, [response]);
+
+    return (
+        <Button
+            disabled={!request}
+            title="Login"
+            onPress={() => {
+                promptAsync();
+                console.log("Login")
+            }}
+        />
+    );
+}
