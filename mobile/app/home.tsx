bfe6e7059b5a07b0d9578ec65040bdc2d6853f95
Mathéo Martin <matheo.martin@epitech.eu>
2024-01-15 10:31:18 +0100

    FRONT + CICD: Fix some things
    MINOR

--- mobile/app/home.tsx
+++ mobile/app/home.tsx
@@ -1,11 +1,11 @@
 import React, {useEffect, useState} from "react";
 import {
-    Image,
-    SafeAreaView,
-    Text,
-    TextInput,
-    TouchableOpacity,
-    View,
+  Image,
+  SafeAreaView,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
 } from "react-native";
 import {LinearGradient} from "expo-linear-gradient";
 import {useNavigation} from "expo-router";
@@ -26,63 +26,63 @@ const StyledTouchableOpacity = styled(TouchableOpacity);
 styled(SafeAreaView);
 
 const Children = () => {
-    const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
-    const [search, setSearch] = useState<string>("");
-    const router = useNavigation();
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [search, setSearch] = useState<string>("");
+  const router = useNavigation();
 
-    return (
-        <SafeAreaView style={{flex: 1}}>
-            <TopBar title="Home" router={router}/>
-            <SearchBar
-                platform="ios"
-                searchIcon={{size: 24}}
-                containerStyle={{backgroundColor: "transparent", top: "0%", marginBottom: "-3%"}}
-                inputContainerStyle={{backgroundColor: colors.light.secondary, borderRadius: 50}}
-                inputStyle={{color: "white", fontSize: 20}}
-                leftIconContainerStyle={{}}
-                rightIconContainerStyle={{}}
-                loadingProps={{animating: true}}
-                onChangeText={newVal => {
-                    setSearch(newVal);
-                }}
-                onClear={() => {
-                    setSearch("")
-                }}
-                placeholder="Search an area..."
-                placeholderTextColor={colors.light.fourthly}
-                showCancel
-                cancelButtonTitle="Cancel"
-                cancelButtonProps={{}}
-                onCancel={() => {
-                    setSearch("")
-                }}
-                value={search}
-            />
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <TopBar title="Home" router={router}/>
+      <SearchBar
+        platform="ios"
+        searchIcon={{size: 24}}
+        containerStyle={{backgroundColor: "transparent", top: "0%", marginBottom: "-3%"}}
+        inputContainerStyle={{backgroundColor: colors.light.secondary, borderRadius: 50}}
+        inputStyle={{color: "white", fontSize: 20}}
+        leftIconContainerStyle={{}}
+        rightIconContainerStyle={{}}
+        loadingProps={{animating: true}}
+        onChangeText={newVal => {
+          setSearch(newVal);
+        }}
+        onClear={() => {
+          setSearch("")
+        }}
+        placeholder="Search an area..."
+        placeholderTextColor={colors.light.fourthly}
+        showCancel
+        cancelButtonTitle="Cancel"
+        cancelButtonProps={{}}
+        onCancel={() => {
+          setSearch("")
+        }}
+        value={search}
+      />
 
-            <AreasList search={search}/>
+      <AreasList search={search}/>
 
-            <BravmmModal
-                animationType="slide"
-                transparent={true}
-                visible={isModalOpen}
-                style={{flex: 1, position: "absolute", top: 0, left: 0}}
-                onRequestClose={() => {
-                    setIsModalOpen(false);
-                }}
-            >
-                <SelectServices/>
-            </BravmmModal>
-            <BottomPlusButton setIsButtonTouched={setIsModalOpen}/>
-        </SafeAreaView>
-    );
+      <BravmmModal
+        animationType="slide"
+        transparent={true}
+        visible={isModalOpen}
+        style={{flex: 1, position: "absolute", top: 0, left: 0}}
+        onRequestClose={() => {
+          setIsModalOpen(false);
+        }}
+      >
+        <SelectServices/>
+      </BravmmModal>
+      <BottomPlusButton setIsButtonTouched={setIsModalOpen}/>
+    </SafeAreaView>
+  );
 }
 
 const Home = () => {
-    return (
-        <AnimatedBackground>
-            <Children/>
-        </AnimatedBackground>
-    );
+  return (
+    <AnimatedBackground>
+      <Children/>
+    </AnimatedBackground>
+  );
 };
 
 export default withExpoSnack(Home);
